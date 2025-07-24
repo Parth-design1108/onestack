853b465633c994b963d686a349b2c315532d2a31
Parth Bhayani <bhayaniparth932@gmail.com>
2025-07-24 09:05:42 +0530

    First Commit

---
+++ string.js
@@ -0,0 +1,13 @@
+// let user="  Parth  ";
+// console.log(user.length)
+// console.log(user.toUpperCase());
+// console.log(user.toLowerCase());
+// console.log(user.charAt(12));
+// console.log(user.indexOf('l'))
+
+// let user1 ="AYUSH%PARTH%SIDDHESH%SAHIL"
+// console.log(user1.split("%"))
+
+let user ="parth";
+console.log(user.substring(-2,3));
+console.log(user.slice(2,3));  
